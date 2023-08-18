fce8dcb6935e14a865e62677ffd89d79dc5f4e03
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-18 10:25:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// config
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config({
+    path: 'config/.env',
+  });
+}
+
+module.exports = app;
